142a98debd81f0f796b71b1bbcdf066da1483d1a
gaelxxl34 <gongoriko10@gmail.com>
2025-08-28 19:17:07 +0300

    Refactor code structure for improved readability and maintainability

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,34 +1,39 @@
-  <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between text-center sm:text-left">
-                <a href="/" class="flex items-center justify-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <img src="images/logo.jpeg" class="h-16" alt="Flowbite Logo" />
-                    {{-- <div class="flex flex-col text-center sm:text-left">
-                        <span class="text-lg font-bold text-gray-900 dark:text-white">Bora Technology</span>
-                        <span class="text-sm text-gray-500 dark:text-gray-400">Engineering a better Africa</span>
-                    </div> --}}
-                </a>
-                <ul class="flex flex-wrap items-center justify-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 social-icons">
-                    <li class="me-4 md:me-6">
-                        <a target="_blank" href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2FBora-Technology-105578237783765%2F" class="hover:underline text-gray-500 dark:text-gray-400">
-                            <i class="fab fa-facebook-f text-xl"></i>
-                        </a>
-                    </li>
-                    <li class="me-4 md:me-6">
-                        <a target="_blank" href="https://www.instagram.com/boratechnology" class="hover:underline text-gray-500 dark:text-gray-400">
-                            <i class="fab fa-instagram text-xl"></i>
-                        </a>
-                    </li>
-                    <li class="me-4 md:me-6">
-                        <a target="_blank" href="https://twitter.com/BoraTechnology" class="hover:underline text-gray-500 dark:text-gray-400">
-                             <i class="fab fa-twitter text-xl"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-           <center>
-             <span style="color: black" class="block text-sm text-gray-500 sm:text-center dark:text-black">© 2024 <a href="" class="hover:underline">Bora Technology</a>. All Rights Reserved.</span>
-           </center>
+<footer class="bg-white m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between text-center sm:text-left text-black">
+            <a href="/" class="flex items-center justify-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="images/logo.jpeg" class="h-16" alt="Flowbite Logo" />
+                {{-- <div class="flex flex-col text-center sm:text-left">
+                    <span class="text-lg font-bold text-black">Bora Technology</span>
+                    <span class="text-sm text-black">Engineering a better Africa</span>
+                </div> --}}
+            </a>
+            <ul
+                class="flex flex-wrap items-center justify-center mb-6 text-sm font-medium text-black sm:mb-0 social-icons">
+                <li class="me-4 md:me-6">
+                    <a target="_blank"
+                        href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2FBora-Technology-105578237783765%2F"
+                        class="hover:underline text-black">
+                        <i class="fab fa-facebook-f text-xl"></i>
+                    </a>
+                </li>
+                <li class="me-4 md:me-6">
+                    <a target="_blank" href="https://www.instagram.com/boratechnology"
+                        class="hover:underline text-black">
+                        <i class="fab fa-instagram text-xl"></i>
+                    </a>
+                </li>
+                <li class="me-4 md:me-6">
+                    <a target="_blank" href="https://twitter.com/BoraTechnology" class="hover:underline text-black">
+                        <i class="fab fa-twitter text-xl"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
-    </footer>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <center>
+            <span class="block text-sm text-black sm:text-center">© 2024 <a href=""
+                    class="hover:underline text-black">Bora Technology</a>. All Rights Reserved.</span>
+        </center>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,66 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta property="og:title" content="Bora Technology">
-    <meta property="og:description" content="Transforming education through immersive VR labs for schools and institutions.">
+    <meta property="og:description"
+        content="Transforming education through immersive VR labs for schools and institutions.">
     <meta property="og:image" content="images/logo.jpeg">
     <meta property="og:url" content="https://boratechnology.org">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Bora Technology">
     <meta name="twitter:card" content="images/lab.jpg">
     <meta name="twitter:title" content="Bora Technology">
-    <meta name="twitter:description" content="Transforming education through immersive VR labs for schools and institutions.">
+    <meta name="twitter:description"
+        content="Transforming education through immersive VR labs for schools and institutions.">
     <meta name="twitter:image" content="images/logo.jpeg">
 
-        <!-- Favicon -->
+    <!-- Favicon -->
     <link href="images/logo.jpeg" rel="icon">
     <link rel="stylesheet" href="css/welcome.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
     <title>Welcome</title>
 
+    <style>
+        body,
+        html {
+            font-family: 'Times New Roman', Times, serif;
+        }
+    </style>
 </head>
+
 <body>
 
- 
+
     <div style="margin-bottom: 50px;">
-         @include('partials.navbar')
+        @include('partials.navbar')
     </div>
 
-    @include('partials.slider')
+    @include('partials.home-content')
 
 
-
-    @include('partials.physiclab-description')
-
-    @include('partials.mathlab-description')
-
-    
 
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/6696e44bbecc2fed69266c37/1i2umniqa';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/6696e44bbecc2fed69266c37/1i2umniqa';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
     </script>
     <!--End of Tawk.to Script-->
 
-    
 
-  
-    @include('partials.dialogbox')
+
+
     @include('partials.footer')
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
 
 </body>
+
 </html>
